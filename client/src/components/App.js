5f42f6f59cfc797426deee1b9c943f588088b197
Michael <michaeltopmiller@gmail.com>
2018-06-29 21:24:49 -0700

    setup react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from '../components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        App
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Home} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
