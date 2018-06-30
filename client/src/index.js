dec937dbb0fcf25d54a013769ccc5a4f8765a7da
Michael <michaeltopmiller@gmail.com>
2018-06-29 21:07:45 -0700

    added Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers/index';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import results from '../reducers/ResultsReducer'
+
+export default combineReducers({
+	results
+})
